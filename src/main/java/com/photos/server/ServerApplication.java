4312d2162207f699385c88c6780975f208a12f5e
fadedBlack <inkeetcha234@gmail.com>
2026-03-04 12:09:21 +0530

    [Inkeet] Adds ssh connection logic to the server;

--- src/main/java/com/photos/server/ServerApplication.java
+++ src/main/java/com/photos/server/ServerApplication.java
@@ -1,7 +1,10 @@
 package com.photos.server;
 
+import com.photos.server.services.SshConnectionService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
--- src/main/java/com/photos/server/controllers/ServerController.java
+++ src/main/java/com/photos/server/controllers/ServerController.java
@@ -4,9 +4,13 @@ import com.photos.server.dto.Photo;
 import com.photos.server.dto.request.MetadataRequestBody;
 import com.photos.server.dto.request.PhotoRequestBody;
 import java.io.IOException;
+
+import com.photos.server.services.SshConnectionService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(("/inkeet/server/photos"))
 public class ServerController {
+
+    @Autowired
+    private final SshConnectionService sshConnectionService;
+
+    public ServerController(SshConnectionService sshConnectionService) {
+        this.sshConnectionService = sshConnectionService;
+    }
 
     @PostMapping("/upload-photo")
     public ResponseEntity<String> uploadPhoto(@ModelAttribute @Validated PhotoRequestBody requestBody)
@@ -30,5 +41,12 @@ public class ServerController {
     public ResponseEntity<String> uploadMetadata(@RequestBody @Validated MetadataRequestBody request) {
         log.info("Received metadata upload request for photoId: {}", request.getTitle());
         return ResponseEntity.ok("Metadata uploaded successfully for photoId: " + request.getTitle());
+    }
+
+    @GetMapping("/connect")
+    public ResponseEntity<String> connect() throws IOException {
+        log.info("Received connection request");
+        sshConnectionService.connect();
+        return ResponseEntity.ok("Connection successful");
     }
 }
