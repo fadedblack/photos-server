8bbf799145ce9aa679a4e36c0c2863132ef2156e
fadedBlack <inkeetcha234@gmail.com>
2026-03-05 17:39:45 +0530

    [Inkeet] Updates build.gradle to call spotless before build is called; Adds an endpoint to view the photo;

--- src/main/java/com/photos/server/ServerApplication.java
+++ src/main/java/com/photos/server/ServerApplication.java
@@ -1,10 +1,7 @@
 package com.photos.server;
 
-import com.photos.server.services.SshConnectionService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
--- src/main/java/com/photos/server/controllers/ServerController.java
+++ src/main/java/com/photos/server/controllers/ServerController.java
@@ -3,15 +3,15 @@ package com.photos.server.controllers;
 import com.photos.server.dto.Photo;
 import com.photos.server.dto.request.MetadataRequestBody;
 import com.photos.server.dto.request.PhotoRequestBody;
-import java.io.IOException;
-
 import com.photos.server.services.SshConnectionService;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +48,11 @@ public class ServerController {
         log.info("Received connection request");
         sshConnectionService.connect();
         return ResponseEntity.ok("Connection successful");
+    }
+
+    @PostMapping("/view/{photoId}")
+    public ResponseEntity<byte[]> viewPhoto(@PathVariable String photoId) throws IOException {
+        log.info("Received view request for photoId: {}", photoId);
+        return ResponseEntity.ok(sshConnectionService.viewPhoto(photoId));
     }
 }
